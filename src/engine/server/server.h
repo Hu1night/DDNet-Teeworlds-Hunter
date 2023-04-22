0aece2c96683dbe8f7b74f05c40c105821df3e0f
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2023-04-22 06:29:17 +0000

    修复warning

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -152,7 +152,7 @@ public:
 	char m_aCurrentMap[64];
 	unsigned m_CurrentMapCrc;
 	unsigned char *m_pCurrentMapData;
-	int m_CurrentMapSize;
+	unsigned int m_CurrentMapSize;
 
 	CDemoRecorder m_DemoRecorder;
 	CRegister m_Register;
