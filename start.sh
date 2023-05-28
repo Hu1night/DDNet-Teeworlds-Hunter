26c6f55c89243775c2df9e298108135c8fc87fae
Hu1night <2506405864@qq.com>
2023-05-28 15:28:31 +0800

    什么鬼情况

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true
-do
-    ./hunter_srv "logfile log.txt"
-    nowtime=$(date +"%Y-%m-%d_%H：%M：%S")
-    mv log.txt log-$nowtime.txt
-    mkdir log
-    mv ./log-$nowtime.txt ./log
-done
